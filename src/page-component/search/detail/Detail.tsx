8ad9fba14a8721430c51191d6a56acca4d0baf98
xlz122 <122874722@qq.com>
2022-12-09 15:20:52 +0800

    feat: 完成搜索详情页面

--- src/page-component/search/detail/Detail.tsx
+++ src/page-component/search/detail/Detail.tsx
@@ -1,8 +1,82 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Image from 'next/image';
 import styles from './detail.module.scss';
 
 function SearchDetail(): React.ReactElement {
-  return <div className={styles.searchDetail}>搜索详情</div>;
+  const [tab, setTab] = useState({
+    list: [
+      {
+        title: '综合',
+        type: 'video',
+        children: [
+          { title: '默认排序', type: 'totalrank' },
+          { title: '播放多', type: 'click' },
+          { title: '新发布', type: 'pubdate' },
+          { title: '弹幕多', type: 'dm' }
+        ]
+      },
+      { title: '番剧', type: 'media_ft' },
+      { title: 'UP主', type: 'media_bangumi' },
+      { title: '影视', type: 'bili_user' }
+    ],
+    index: 0,
+    orderIndex: 0,
+    type: 'video',
+    order: 'totalrank'
+  });
+
+  const tabChange = (index: number, type: string): void => {
+    setTab({ ...tab, type, index, orderIndex: 0, order: 'totalrank' });
+  };
+
+  const orderChange = (index: number, type: string): void => {
+    setTab({ ...tab, order: type, orderIndex: index });
+  };
+
+  return (
+    <div className={styles.searchDetail}>
+      <div className={styles.tab}>
+        {tab?.list?.map((item, index) => {
+          return (
+            <div
+              className={`${styles.tabItem} ${
+                tab.index === index ? styles.tabActiveItem : ''
+              }`}
+              key={index}
+              onClick={() => tabChange(index, item.type)}
+            >
+              {item.title}
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles.orderTab}>
+        {tab?.list[tab.index]?.children?.map((item, index) => {
+          return (
+            <div
+              className={`${styles.orderItem} ${
+                tab.orderIndex === index ? styles.orderActiveItem : ''
+              }`}
+              key={index}
+              onClick={() => orderChange(index, item.type)}
+            >
+              {item.title}
+            </div>
+          );
+        })}
+      </div>
+      <div className={styles.notFound}>
+        <Image
+          className={styles.notFoundImage}
+          width={239}
+          height={149}
+          src={'/images/search/not-found.png'}
+          alt=""
+        />
+        <p className={styles.notFoundText}>什么都没有找到啊 T_T</p>
+      </div>
+    </div>
+  );
 }
 
 export default SearchDetail;
