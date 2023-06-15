7ae053efb5b15de5164bdfcd4e59ee76e69ebc62
xlz122 <122874722@qq.com>
2023-06-15 11:10:10 +0800

    feat: 完成视频详情-视频播放

--- src/page-component/video/video-player/VideoPlayer.tsx
+++ src/page-component/video/video-player/VideoPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { useSearchParams } from 'next/navigation';
 import Image from 'next/image';
 import { timeStampToDuration, formatTenThousand } from '@utils/utils';
@@ -44,33 +44,54 @@ function VideoPlayer(props: Props): React.ReactElement {
     getVideoPlayurl();
   }, [aid]);
 
+  // 播放状态
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  const handleVideoPress = () => {
+    setPlaying(true);
+  };
+
+  useEffect(() => {
+    playing && videoRef.current?.play();
+  }, [playing]);
+
   return (
     <div className={styles.videoPlayer}>
-      <video className={styles.video} src={videoSrc} controls={true} />
-      <div className={styles.cover}>
-        <Image src={props.pic} fill sizes="100%" priority alt="" />
-        <div className={styles.coverIconPlay}>
-          <Image
-            src={'/images/video/icon-play.png'}
-            fill
-            sizes="100%"
-            priority
-            alt=""
-          />
+      {playing && (
+        <video
+          className={styles.video}
+          controls
+          ref={videoRef}
+          src={videoSrc}
+        />
+      )}
+      {!playing && (
+        <div className={styles.cover}>
+          <Image src={props.pic} fill sizes="100%" priority alt="" />
+          <div className={styles.coverIconPlay} onClick={handleVideoPress}>
+            <Image
+              src={'/images/video/icon-play.png'}
+              fill
+              sizes="100%"
+              priority
+              alt=""
+            />
+          </div>
+          <div className={styles.coverTip}>
+            <span className={styles.tipText}>
+              {formatTenThousand(props.stat.view)}万观看
+            </span>
+            <span className={styles.tipText}>{props.stat.danmaku}弹幕</span>
+            <span className={styles.tipText}>
+              {formatDateTime(props?.ctime, 'MM-dd')}
+            </span>
+            <span className={styles.tipTime}>
+              {timeStampToDuration(props.duration)}
+            </span>
+          </div>
         </div>
-        <div className={styles.coverTip}>
-          <span className={styles.tipText}>
-            {formatTenThousand(props.stat.view)}万观看
-          </span>
-          <span className={styles.tipText}>{props.stat.danmaku}弹幕</span>
-          <span className={styles.tipText}>
-            {formatDateTime(props?.ctime, 'MM-dd')}
-          </span>
-          <span className={styles.tipTime}>
-            {timeStampToDuration(props.duration)}
-          </span>
-        </div>
-      </div>
+      )}
     </div>
   );
 }
