5e7f8ab4a818485856783ff76085844f482923fb
xlz122 <122874722@qq.com>
2023-06-15 10:32:48 +0800

    feat: 完成直播分类列表页面

--- src/api/live.ts
+++ src/api/live.ts
@@ -25,6 +25,37 @@ export const liveArea = ({ baseUrl }: BaseParams): AxiosPromise => {
   });
 };
 
+type LiveAreaList = BaseParams & {
+  parent_area_id: number;
+  area_id: number;
+  page: number;
+  size: number;
+};
+
+/**
+ * @description 直播分类列表
+ * @param { String } [baseUrl] - 接口基础url(服务端渲染)
+ * @param { Number } parent_area_id - 分类id
+ * @param { Number } area_id - 默认传0
+ * @param { Number } page - 页数
+ * @param { Number } size - 条数
+ */
+export const liveAreaList = ({
+  baseUrl,
+  parent_area_id,
+  area_id,
+  page,
+  size
+}: LiveAreaList): AxiosPromise => {
+  const params = { parent_area_id, area_id, page, size };
+
+  return axios.request({
+    url: `${baseUrl ? baseUrl : '/api'}/live/area/list`,
+    method: 'get',
+    params
+  });
+};
+
 type LiveInfo = BaseParams & {
   roomid: number;
 };
